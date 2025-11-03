c399a5b6021b35aff6e2f1db9bbed95a8fe5568b
Sara <sara@saras-MacBook-Air.local>
2025-11-03 18:47:39 +0300

    SQL solution

--- answer.sql
+++ answer.sql
@@ -1,1 +1,13 @@
--- ADD YOUR SQL QUERY HERE
+SELECT
+  it.investor_id,
+  s.sector_name,
+  ROUND(
+    100.0 * SUM(it.no_of_shares) /
+    (SELECT SUM(no_of_shares)
+     FROM investor_transactions t
+     WHERE t.investor_id = it.investor_id)
+  , 2) AS percentage
+FROM investor_transactions it
+JOIN sectors s ON s.sector_id = it.sector_id
+GROUP BY it.investor_id, s.sector_name
+ORDER BY it.investor_id, s.sector_name;
