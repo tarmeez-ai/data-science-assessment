2be596c63b3e6d3719e44b6eae183a8f5db73cc7
Renad Naser <87290960+re-compsci@users.noreply.github.com>
2025-11-17 14:59:55 +0300

    soultion

--- answer.sql
+++ answer.sql
@@ -1,1 +1,5 @@
--- ADD YOUR SQL QUERY HERE
+Select inv.investor_id, sec.sector_name,     
+round((inv.no_of_shares * 100.0 / sum(inv.no_of_shares) over (partition by inv.investor_id)),2) as share_percentage
+From investor_transactions inv
+join sectors sec on inv.sector_id = sec.sector_id
+GROUP BY sec.sector_name;
